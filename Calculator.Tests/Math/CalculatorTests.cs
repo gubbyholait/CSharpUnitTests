5a4f3c4b294c4de1418c922d89ff34a5b6ad6ef1
gubbyholait <157219684+gubbyholait@users.noreply.github.com>
2025-03-16 12:13:39 +0000

    Add files via upload

---
+++ Calculator.Tests/Math/CalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculator.Tests.Math
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        // Naming convention MethodName.Scenario.ExpectedOutcome
+        [TestMethod]
+        public void Add_AddTwoWholeNumbers_SumofThoseNumbers()
+        {
+            // Arrange 
+            var calculator = new UnitTestCSharp.Math.Calculator();
+
+            //Act
+            int res = calculator.Add(45, 55);
+
+            //Assert
+            Assert.AreEqual(res, 100);
+        }
+
+        [TestMethod]
+        public void Add_SubtractTwoWholeNumbers_SumofThoseNumbers()
+        {
+            // Arrange 
+            var calculator = new UnitTestCSharp.Math.Calculator();
+
+            //Act
+            int res = calculator.Subtract(55, 45);
+
+            //Assert
+            Assert.AreEqual(res, 10);
+        }
+    }
+}
---
+++ Calculator.Tests/Strings/EmailTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests.Strings
+{
+    [TestClass]
+    public class EmailTests
+    {
+        // Naming convention MethodName.Scenario.ExpectedOutcome
+        [TestMethod]
+        public void IsValid_ValidEmailAddress_ReturnTrue()
+        {
+            // Arrange 
+            var emailValidator = new UnitTestCSharp.Strings.EmailValidaotr();
+
+            //Act
+            bool res = emailValidator.IsValidEmail("gholait@hotmail.co.uk");
+
+            //Assert
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void IsValid_EmptyEmail_ReturnFalse()
+        {
+            // Arrange 
+            var emailValidator = new UnitTestCSharp.Strings.EmailValidaotr();
+
+            //Act
+            bool res = emailValidator.IsValidEmail("");
+
+            //Assert
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsValid_EmailWithNoAtSign_ReturnFalse()
+        {
+            // Arrange 
+            var emailValidator = new UnitTestCSharp.Strings.EmailValidaotr();
+
+            //Act
+            bool res = emailValidator.IsValidEmail("gubbyholait.com");
+
+            //Assert
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsValid_EmailWithNoPeriod_ReturnFalse()
+        {
+            // Arrange 
+            var emailValidator = new UnitTestCSharp.Strings.EmailValidaotr();
+
+            //Act
+            bool res = emailValidator.IsValidEmail("gubbyholait@com");
+
+            //Assert
+            Assert.IsFalse(res);
+        }
+    }
+}
