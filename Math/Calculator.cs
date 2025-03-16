dc46eb01aeaff6457b59c79319ca36d360e91374
gubbyholait <157219684+gubbyholait@users.noreply.github.com>
2025-03-16 11:52:28 +0000

    Add files via upload

---
+++ Math/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace UnitTestCSharp.Math
+{
+    public class Calculator
+    {
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public int Subtract(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public double Divide(double firstNumber, double secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+
+        public double Multiply(double firstNumber, double secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+    }
+}
